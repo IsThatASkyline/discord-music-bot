ad3a0503eb2d261d9a05815d91556474212214c1
artklk12 <artem-kulik@list.ru>
2022-11-27 18:38:48 +0300

    its workin

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.8
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+RUN apt-get update && apt-get install -y ffmpeg
+
+EXPOSE 8000
+CMD ["python", "main.py", "0.0.0.0:8000"]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from youtube_dl import YoutubeDL
-import Config
+
 
 YDL_OPTIONS = {'format': 'worstaudio/best', 'noplaylist': 'False', 'simulate': 'True', 'key': 'FFmpegExtractAudio'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
@@ -11,15 +11,24 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='-', intents=intents)
 
+players = {}
+queue = []
+
+def check_queue(ctx, vc):
+    if queue != []:
+        link = queue.pop(0)
+        vc.play(discord.FFmpegPCMAudio(source=link, **FFMPEG_OPTIONS),
+                after=lambda e: check_queue(ctx, vc))
+
 
 @bot.command()
 async def play(ctx, url):
+    vc = ctx.voice_client
+    if vc is None:
+        vc = await ctx.message.author.voice.channel.connect()
     if ctx.author.voice is None:
         return await ctx.send("Ну ты и конченный, сначала надо зайти на канал.")
-    elif ctx.guild.voice_client:
-        return await ctx.send("Уже играет нереальный трек, жди пока он закончится")
-    else:
-        vc = await ctx.message.author.voice.channel.connect()
+    elif ctx.guild.voice_client and ctx.voice_client.is_playing() is True:
 
         with YoutubeDL(YDL_OPTIONS) as ydl:
             if 'https://' in url:
@@ -28,8 +37,21 @@ async def play(ctx, url):
                 info = ydl.extract_info(f'ytsearch:{url}', download=False)['entries'][0]
 
         link = info['formats'][0]['url']
+        queue.append(link)
+        return await ctx.send("Добавил твой трек в очередь")
 
-        vc.play(discord.FFmpegPCMAudio(executable='ffmpeg', source=link, **FFMPEG_OPTIONS))
+    else:
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            if 'https://' in url:
+                info = ydl.extract_info(url, download=False)
+            else:
+                info = ydl.extract_info(f'ytsearch:{url}', download=False)['entries'][0]
+
+        link = info['formats'][0]['url']
+
+        vc.play(discord.FFmpegPCMAudio(source=link, **FFMPEG_OPTIONS), after=lambda e: check_queue(ctx, vc))
+
+
 
 @bot.command()
 async def stop(ctx):
@@ -38,4 +60,4 @@ async def stop(ctx):
     else:
         await ctx.send("Я не в канале, твоя мать в канаве")
 
-bot.run(Config.token)
+bot.run(TOKEN)
